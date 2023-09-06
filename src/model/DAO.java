b733d5df41eedb36df2058405124bb996120ce8c
FELIPE DE ALMEIDA DUARTE <felipe.aduarte2@senacsp.edu.br>
2023-09-06 15:18:28 -0300

    atualização definitiva

--- src/model/DAO.java
+++ src/model/DAO.java
@@ -5,22 +5,16 @@ import java.sql.DriverManager;
 
 public class DAO {
 
-	// Variáveis para configurar o banco de dados
 	private String driver = "com.mysql.cj.jdbc.Driver";
 	private String url = "jdbc:mysql://127.0.0.1:3306/dbsistema";
 	private String user = "root";
 	private String password = "";
-	// Criação de um objeto para uso da classe Connection(JDBC)
+	
 	private Connection con;
 
-	/**
-	 * Método responsável por abrir a conexão com o banco
-	 * @return con
-	 */
 	public Connection conectar() {
-		// tratamento de exceções
 		try {
-			// as linhas abaixo abrem a conexão com o banco
+			
 			Class.forName(driver);
 			con = DriverManager.getConnection(url, user, password);
 			return con;
